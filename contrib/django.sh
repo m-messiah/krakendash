85873221e03abaa41ca74da9af3d0873173b327a
m_messiah <m.muzafarov@gmail.com>
2014-09-17 11:47:09 +0600

    patch for launch in centos

--- contrib/django.sh
+++ contrib/django.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-screen -dmS django python krakendash/manage.py runserver 0.0.0.0:8000
+screen -dmS django python krakendash/manage.py runserver 0.0.0.0:8000 || scl enable python27 'python krakendash/manage.py runserver 0.0.0.0:8000' > kraken.log 2>&1 &
